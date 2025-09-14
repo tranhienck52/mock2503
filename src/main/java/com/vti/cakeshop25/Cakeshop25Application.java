d51e433cbcb5d479a4f9834dc0047ceac17179ec
tranhienck52 <tranhienck52@gmail.com>
2025-09-14 21:06:19 +0900

    [Add] cakeshop project

---
+++ src/main/java/com/vti/cakeshop25/Cakeshop25Application.java
@@ -0,0 +1,13 @@
+package com.vti.cakeshop25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cakeshop25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cakeshop25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vti/cakeshop25/Cakeshop25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.cakeshop25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cakeshop25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
